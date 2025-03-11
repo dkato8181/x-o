046c7eec2ad458a1d8c37ed2933c1d26b2ad4477
davidk <dkato81@gmail.com>
2025-03-11 20:10:49 +0300

    add turn indicator board

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
 
 <template>
   <main class="flex flex-col items-center justify-center bg-sky-100 h-screen">
-    <div class="grid grid-cols-3 gap-1 my-9">
+    <div class="grid grid-cols-2 gap-1 mb-6">
+      <div class="bg-rose-700 p-12 text-white text-center text-7xl">X</div>
+      <div class="bg-rose-700 p-12 text-white text-center text-7xl">O</div>
+    </div>
+    <div class="grid grid-cols-3 gap-1 mt-6">
       <div class="bg-sky-700 p-8 text-white text-center text-7xl">01</div>
       <div class="bg-sky-700 p-8 text-white text-center text-7xl">02</div>
       <div class="bg-sky-700 p-8 text-white text-center text-7xl">03</div>
