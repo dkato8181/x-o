daede6c92b3ff03a6e9a08925ff53da492bf6ffb
davidk <dkato81@gmail.com>
2025-03-11 21:13:20 +0300

    add tile component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Tile from './components/Tile.vue'
+</script>
 
 <template>
   <main class="flex flex-col items-center justify-center bg-sky-100 h-screen">
@@ -7,15 +9,7 @@
       <div class="bg-rose-700 p-12 text-white text-center text-7xl">O</div>
     </div>
     <div class="grid grid-cols-3 gap-1 mt-6">
-      <div class="bg-sky-700 p-8 text-white text-center text-7xl">01</div>
-      <div class="bg-sky-700 p-8 text-white text-center text-7xl">02</div>
-      <div class="bg-sky-700 p-8 text-white text-center text-7xl">03</div>
-      <div class="bg-sky-700 p-8 text-white text-center text-7xl">04</div>
-      <div class="bg-sky-700 p-8 text-white text-center text-7xl">05</div>
-      <div class="bg-sky-700 p-8 text-white text-center text-7xl">06</div>
-      <div class="bg-sky-700 p-8 text-white text-center text-7xl">07</div>
-      <div class="bg-sky-700 p-8 text-white text-center text-7xl">08</div>
-      <div class="bg-sky-700 p-8 text-white text-center text-7xl">09</div>
+      <Tile v-for="item in [1, 2, 3, 4, 5, 6, 7, 8, 9]" />
     </div>
   </main>
 </template>
---
+++ src/components/Tile.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const card = ref('')
+const turn = ref('X')
+function play(event: any) {
+  card.value = turn.value
+  if (turn.value == 'X') {
+    turn.value = 'O'
+  } else if (turn.value == 'O') {
+    turn.value = 'X'
+  }
+}
+</script>
+
+<template>
+  <div @click="play" class="bg-sky-700 p-8 text-white text-center text-7xl">{{ card }}</div>
+</template>
