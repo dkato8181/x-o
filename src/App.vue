131bee6511b89689821e6d7672f4cb9e5820bbad
davidk <dkato81@gmail.com>
2025-03-11 13:53:41 +0300

    add grid

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <main class="flex flex-col items-center justify-center bg-sky-100 h-screen">
+    <div class="grid grid-cols-3 gap-1 my-9">
+      <div class="bg-sky-700 p-8 text-white text-center text-7xl">01</div>
+      <div class="bg-sky-700 p-8 text-white text-center text-7xl">02</div>
+      <div class="bg-sky-700 p-8 text-white text-center text-7xl">03</div>
+      <div class="bg-sky-700 p-8 text-white text-center text-7xl">04</div>
+      <div class="bg-sky-700 p-8 text-white text-center text-7xl">05</div>
+      <div class="bg-sky-700 p-8 text-white text-center text-7xl">06</div>
+      <div class="bg-sky-700 p-8 text-white text-center text-7xl">07</div>
+      <div class="bg-sky-700 p-8 text-white text-center text-7xl">08</div>
+      <div class="bg-sky-700 p-8 text-white text-center text-7xl">09</div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
