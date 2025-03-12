123d17467250d7c39a0f1b5b46bab52aa31ce152
davidk <dkato81@gmail.com>
2025-03-12 09:10:50 +0300

    add turnboard

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import Tile from './components/Tile.vue'
+import TurnBoard from './components/TurnBoard.vue'
 </script>
 
 <template>
   <main class="flex flex-col items-center justify-center bg-sky-100 h-screen">
-    <div class="grid grid-cols-2 gap-1 mb-6">
-      <div class="bg-rose-700 p-12 text-white text-center text-7xl">X</div>
-      <div class="bg-rose-700 p-12 text-white text-center text-7xl">O</div>
-    </div>
+    <TurnBoard />
     <div class="grid grid-cols-3 gap-1 mt-6">
       <Tile v-for="n in 9" />
     </div>
---
+++ src/components/TurnBoard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { store } from '../store'
+</script>
+
+<template>
+  <div class="grid grid-cols-2 gap-1 mb-6">
+    <div
+      class="bg-rose-600 p-12 text-white text-center text-7xl"
+      :class="{ 'font-bold': store.turn == 'X', 'bg-rose-700': store.turn == 'X' }"
+    >
+      X
+    </div>
+    <div
+      class="bg-rose-600 p-12 text-white text-center text-7xl"
+      :class="{ 'font-bold': store.turn == 'O', 'bg-rose-700': store.turn == 'O' }"
+    >
+      O
+    </div>
+  </div>
+</template>
